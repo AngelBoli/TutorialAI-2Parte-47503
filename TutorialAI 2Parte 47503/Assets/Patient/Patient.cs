15bb11b56156b2dd08e9c1a2b3ca16a1f5f19789
Beatriz Santos <s20bea02@gmail.com>
2022-12-15 14:19:41 +0000

    Working

--- TutorialAI 2Parte 47503/Assets/Patient/Patient.cs
+++ TutorialAI 2Parte 47503/Assets/Patient/Patient.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Patient : GAgent {
 
-    new void Start() {
+    new void Start() 
+    {
         base.Start();
+
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
         goals.Add(s1, 3);
 
